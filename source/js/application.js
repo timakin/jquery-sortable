f5b98dcb20cfad155abde03184cc84cbb1b5e3ac
timakin <timaki.st@gmail.com>
2016-04-16 11:47:39 +0900

    directory path

--- source/js/application.js
+++ source/js/application.js
@@ -1,10 +1,10 @@
-//= require "vendor/jquery"
-//= require "vendor/jquery.color"
-//= require "vendor/bootstrap-switch"
-//= require "vendor/bootstrap-scrollspy"
-//= require "vendor/bootstrap-dropdown"
-//= require "vendor/bootstrap-button"
-//= require "jquery-sortable"
+//= require "./vendor/jquery"
+//= require "./vendor/jquery.color"
+//= require "./vendor/bootstrap-switch"
+//= require "./vendor/bootstrap-scrollspy"
+//= require "./vendor/bootstrap-dropdown"
+//= require "./vendor/bootstrap-button"
+//= require "./jquery-sortable"
 //= require_directory "./examples/"
 
 $(function  () {
